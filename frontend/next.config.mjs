41d611649cde7d7b33827a344f54da5cf4c9dcbf
김형진 <rlagudwls3469@gmail.com>
2024-03-12 14:49:00 +0900

    fix: fix undefined dirname

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,18 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+// next.config.mjs
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(__filename);
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   webpack: (config) => {
+    // eslint-disable-next-line no-param-reassign
     config.resolve.alias['@'] = path.resolve(__dirname, './styles');
     return config;
   },
