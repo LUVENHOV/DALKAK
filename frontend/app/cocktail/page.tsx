0f8d49946f4796b52ae632b1cf2178fb7df4e9b8
유현지 <ccamy0915@naver.com>
2024-03-19 17:30:48 +0900

    feat: Modify cocktail list formation

--- frontend/app/cocktail/page.tsx
+++ frontend/app/cocktail/page.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styles from './cocktail-list.module.scss';
+import CocktailSearchForm from '@/components/cocktail-list/CocktailSearchForm';
+import SortBy from '@/components/cocktail-list/SortBy';
+
+/** cocktail list data fetching */
 
 export default function Page() {
-  return <h1>전체 칵테일 리스트 페이지</h1>;
+  return (
+    <div className={styles['page-container']}>
+      <div className={styles['search-container']}>
+        <CocktailSearchForm />
+      </div>
+      <div className={styles['list-container']}>
+        <div className={styles['sort-container']}>
+          <SortBy />
+        </div>
+        <div className={styles['cocktail-list']}>칵테일 리스트</div>
+      </div>
+    </div>
+  );
 }
