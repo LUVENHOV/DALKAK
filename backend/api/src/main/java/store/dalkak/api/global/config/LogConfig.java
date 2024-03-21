a93fff82296f89daace38724d3c9651d2e534d8f
곽희웅 <gmldnd567@naver.com>
2024-03-21 16:39:14 +0900

    S10P22A407-9 fix: Modify LogConfig

--- backend/api/src/main/java/store/dalkak/api/global/config/LogConfig.java
+++ backend/api/src/main/java/store/dalkak/api/global/config/LogConfig.java
@@ -1,6 +1,8 @@
 package store.dalkak.api.global.config;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @RestController
-@Slf4j(topic = "ek-log")
+@RequiredArgsConstructor
+//@Slf4j
 public class LogConfig {
+    private final Logger logger = LoggerFactory.getLogger("dalkak-log");
 
-    @GetMapping("/log")
-    public ResponseEntity<String> getTestLog() {
-        log.info("test-log");
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/ek-log")
+    public ResponseEntity<String> getElkTestLog() {
+        logger.info("test-log");
         return ResponseEntity.ok("ok");
     }
+
 }
