aa77ce6a5ddf269811cbf77ce8dc8b3243f36254
백천주 <dieswing@naver.com>
2024-03-22 18:23:19 +0900

    S10P22A407-247 test: Add test code in custom cocktail add recipe component

--- frontend/components/custom-cocktail/write/CustomCocktailAddRecipe.tsx
+++ frontend/components/custom-cocktail/write/CustomCocktailAddRecipe.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 import styles from './CustomCocktailAddRecipe.module.scss';
 
@@ -10,9 +11,11 @@ interface Props {
 
 const splitedRecipe = (recipe: string) => recipe.split('|').join('\n\n');
 
-console.log(splitedRecipe);
+// console.log(splitedRecipe);
 
 export default function CustomCocktailAddRecipe({ recipe }: Props) {
+  const pathname = usePathname();
+  console.log(pathname);
   const [inputValue, setInputValue] = useState(recipe);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
