986aaaca2f1c265a1152cd70b418840aed7e488e
김형진 <rlagudwls3469@gmail.com>
2024-03-24 23:49:31 +0900

    fix: fix member state actions

--- frontend/store/memberStore.ts
+++ frontend/store/memberStore.ts
@@ -2,57 +2,56 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-interface MemberActions {
-  setId: (id: string) => void;
-  setNickname: (nickname: string) => void;
-  setBirthDate: (birthDate: string) => void;
-  setGender: (gender: string) => void;
-  setIsLoggedIn: (isLoggedIn: boolean) => void;
-  clearAll: () => void;
-  tmp: () => void;
-}
-
 interface StoreState {
-  actions: MemberActions;
   id: string;
   nickname: string;
   birthDate: string;
   gender: string;
   isLoggedIn: boolean;
+  survey_completion: boolean;
+  setId: (id: string) => void;
+  setNickname: (nickname: string) => void;
+  setBirthDate: (birthDate: string) => void;
+  setGender: (gender: string) => void;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+  setSurveyCompletion: (surveyCompletion: boolean) => void;
+  clearAll: () => void;
 }
 
 const memberStore = create(
   persist<StoreState>(
     (set) => ({
+      // todo : initial state
       id: '',
       nickname: '',
       birthDate: '',
       gender: '',
       isLoggedIn: true,
-      actions: {
-        setId: (id: string) => set({ id }),
-        setNickname: (nickname: string) => set({ nickname }),
-        setBirthDate: (birthDate: string) => set({ birthDate }),
-        setGender: (gender: string) => set({ gender }),
-        setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
-        clearAll: () =>
-          set({
-            id: '',
-            nickname: '',
-            birthDate: '',
-            gender: '',
-            isLoggedIn: false,
-          }),
-
-        tmp: () =>
-          set({
-            id: 'tmp',
-            nickname: '김형진',
-            birthDate: '1998-01-17 ',
-            gender: 'male',
-            isLoggedIn: true,
-          }),
+      survey_completion: false,
+      setId: (id: string) => set({ id }),
+      setNickname: (nickname: string) => set({ nickname }),
+      setBirthDate: (birthDate: string) => set({ birthDate }),
+      setGender: (gender: string) => set({ gender }),
+      setIsLoggedIn: (isLoggedIn: boolean) => set({ isLoggedIn }),
+      setSurveyCompletion(survey_completion) {
+        set({ survey_completion });
       },
+      clearAll: () =>
+        set({
+          id: '',
+          nickname: '',
+          birthDate: '',
+          gender: '',
+          isLoggedIn: false,
+        }),
+      setMemberStateLogin: (id: string, nickname: string, survey_completion: boolean) =>{
+        set({
+          id,
+          nickname,
+          survey_completion,
+          isLoggedIn: true,
+        });
+      }
     }),
     {
       name: 'member',
