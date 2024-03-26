bc74f39755cab6d023473d27c3971d2ace058873
김형진 <rlagudwls3469@gmail.com>
2024-03-26 09:54:08 +0900

    S10P22A407-9 feat: 베이스 입력 설문 페이지 작성

--- frontend/components/survey/BaseSurvey.tsx
+++ frontend/components/survey/BaseSurvey.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
+import CocktailItems from '../surveyComponents/CocktailItems';
 
-function BaseSurvey() {
-  return <div>BaseSurvey</div>;
+const baseList = [
+  {
+    id: 1,
+    name: '샴페인',
+    image: '',
+  },
+  { id: 2, name: '럼', image: '' },
+  { id: 3, name: '위스키', image: '' },
+  { id: 4, name: '보드카', image: '' },
+  { id: 5, name: '진', image: '' },
+  { id: 6, name: '테킬라', image: '' },
+  { id: 7, name: '브랜디', image: '' },
+  { id: 8, name: '리큐어', image: '' },
+  { id: 9, name: '와인', image: '' },
+  { id: 10, name: ' 비터스', image: '' },
+];
+export default function BaseSurvey() {
+  return (
+    <div className="wrapper">
+      <div className="cocktail-grid">
+        {baseList.map((base) => (
+          <CocktailItems
+            key={base.id}
+            id={base.id}
+            name={base.name}
+            image={base.image}
+          />
+        ))}
+        {/* {CocktailSampleList?.map(cocktail)=>(
+          <div key={cocktail.id} className="cocktail-item">
+            <img src=cocktail.image} alt={cocktail.name} />
+            <div>{CocktailSampleList.</div>
+            </div>
+        )} */}
+      </div>
+    </div>
+  );
 }
-export default BaseSurvey;
