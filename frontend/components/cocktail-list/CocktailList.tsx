ab128b5720dcb5307a81e4de0e0da7213c45d980
유현지 <ccamy0915@naver.com>
2024-03-26 14:39:43 +0900

    feat: Modify "cocktail list" page

--- frontend/components/cocktail-list/CocktailList.tsx
+++ frontend/components/cocktail-list/CocktailList.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 import styles from './CocktailList.module.scss';
-import useCocktailList from '@/app/api/cocktail-search';
 import CocktailCard from '@/components/cocktail-list/CocktailCard';
-import searchStore from '@/store/searchStore';
 import useSearchStore from '@/store/searchStore';
 
 interface ICocktailType {
@@ -14,40 +14,89 @@ interface ICocktailType {
   heart_count: number;
 }
 
-interface IListType {
-  cocktailList: ICocktailType[];
+interface ISearchType {
+  page: number;
+  cocktailName: string;
+  ingredients: number[];
+  base: number;
+  minAlcohol: number;
+  maxAlcohol: number;
+  color: number;
+  sweetness: number;
+  orderBy: number;
 }
 
+const authorization =
+  'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MTEzMjkwNDUsImV4cCI6MTcxMTc2MTA0NSwiaWQiOjN9.zcY6r5AdHWBddd-sUz8oFdGV14DZLLyXi_5-BG--C20';
+
+const getCocktailList = async ({
+  page,
+  cocktailName,
+  ingredients,
+  base,
+  minAlcohol,
+  maxAlcohol,
+  color,
+  sweetness,
+  orderBy,
+}: ISearchType) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/cocktails/search?page=${page}&cocktail-name=${cocktailName}&ingredients=${ingredients}&base=${base}&min-alcohol=${minAlcohol}&max-alcohol=${maxAlcohol}&color=${color}&sweetness=${sweetness}&orderBy=${orderBy}`,
+    {
+      headers: { authorization },
+      next: { tags: ['cocktailList'] },
+    },
+  );
+  const json = await res.json();
+  console.log(json.data.cocktails);
+  return (await json).data.cocktails;
+};
+
 export default function CocktailList() {
-  // const {
-  //   page,
-  //   cocktailName,
-  //   ingredients,
-  //   base,
-  //   minAlcohol,
-  //   maxAlcohol,
-  //   color,
-  //   sweetness,
-  //   orderBy,
-  // } = useSearchStore();
+  const {
+    page,
+    cocktailName,
+    ingredients,
+    base,
+    minAlcohol,
+    maxAlcohol,
+    color,
+    sweetness,
+    orderBy,
+  } = useSearchStore();
 
-  const authorization =
-    'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MTEzMjkwNDUsImV4cCI6MTcxMTc2MTA0NSwiaWQiOjN9.zcY6r5AdHWBddd-sUz8oFdGV14DZLLyXi_5-BG--C20';
-
-  const cocktailList = useCocktailList(authorization);
-  // const res = fetch(
-  //   `${process.env.NEXT_PUBLIC_BASE_URL}/cocktails/search?page=${page}&cocktail-name=${cocktailName}&ingredients=${ingredients}&base=${base}&min-alcohol=${minAlcohol}&max-alcohol=${maxAlcohol}&color=${color}&sweetness=${sweetness}&orderBy=${orderBy}`,
-  //   {
-  //     headers: { authorization },
-  //     next: { tags: ['cocktailList'] },
-  //   },
-  // );
-  // const cocktailList = res;
-  console.log(cocktailList);
+  const [cocktailList, setCocktailList] = useState([]);
+  useEffect(() => {
+    const updateViews = async () => {
+      const cocktailRes = await getCocktailList({
+        page,
+        cocktailName,
+        ingredients,
+        base,
+        minAlcohol,
+        maxAlcohol,
+        color,
+        sweetness,
+        orderBy,
+      });
+      setCocktailList(cocktailRes);
+    };
+    updateViews();
+  }, [
+    page,
+    cocktailName,
+    ingredients,
+    base,
+    minAlcohol,
+    maxAlcohol,
+    color,
+    sweetness,
+    orderBy,
+  ]);
 
   return (
     <div className={styles['cocktail-list']}>
-      {/* {cocktailList.map((cocktail: ICocktailType) => (
+      {cocktailList.map((cocktail: ICocktailType) => (
         <CocktailCard
           key={cocktail.id}
           id={cocktail.id}
@@ -56,7 +105,7 @@ export default function CocktailList() {
           image={cocktail.image}
           heartCount={cocktail.heart_count}
         />
-      ))} */}
+      ))}
     </div>
   );
 }
