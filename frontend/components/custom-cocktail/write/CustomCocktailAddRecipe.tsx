06c05fc5e026607fb66ab75da58be9e23a439112
백천주 <dieswing@naver.com>
2024-03-29 00:00:14 +0900

    feat: Add props in custom cocktail add recipe component

--- frontend/components/custom-cocktail/write/CustomCocktailAddRecipe.tsx
+++ frontend/components/custom-cocktail/write/CustomCocktailAddRecipe.tsx
@@ -1,30 +1,38 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 
 import styles from './CustomCocktailAddRecipe.module.scss';
 
 interface Props {
   recipe: string;
+  inputValue: string;
+  handleInputChange: ChangeEvent;
+  splitedRecipe: string;
 }
 
-const splitedRecipe = (recipe: string) => recipe.split('|').join('\n\n');
+// const splitedRecipe = (recipe: string) => recipe.split('|').join('\n\n');
 
-export default function CustomCocktailAddRecipe({ recipe }: Props) {
-  const [inputValue, setInputValue] = useState(' ');
+export default function CustomCocktailAddRecipe({
+  handleInputChange,
+  recipe,
+  inputValue,
+  splitedRecipe,
+}: Props) {
+  // const [inputValue, setInputValue] = useState(' ');
 
-  useEffect(() => {
-    setInputValue(recipe);
-  }, [recipe]);
+  // useEffect(() => {
+  //   setInputValue(recipe);
+  // }, [recipe]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInputValue(e.target.value);
-  };
+  // const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  //   setInputValue(e.target.value);
+  // };
   return (
     <div>
       <div className={styles.title}>레시피</div>
       <textarea
-        value={inputValue && splitedRecipe(inputValue)}
+        value={splitedRecipe}
         className={styles['input-style']}
         onChange={handleInputChange}
       />
