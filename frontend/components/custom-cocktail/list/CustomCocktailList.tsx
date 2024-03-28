f4663a48b4989a595a7e369f48897b919462e640
백천주 <dieswing@naver.com>
2024-03-28 14:40:14 +0900

    S10P22A407-315 feat: Modify custom cocktail list and pagination components structure

--- frontend/components/custom-cocktail/list/CustomCocktailList.tsx
+++ frontend/components/custom-cocktail/list/CustomCocktailList.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect, useCallback } from 'react';
 
 import UpperLineBanner from '@/components/common/UpperLineBanner';
 import CustomCocktailCardWrapper from '@/components/custom-cocktail/CustomCocktailCardWrapper';
-// import CustomCocktailWriteButton from '@/components/custom-cocktail/CustomCocktailWriteButton';
 import CustomCocktailPagination from '@/components/custom-cocktail/list/CustomCocktailPagination';
 
 interface User {
@@ -19,69 +20,74 @@ interface Custom_Cocktails {
 }
 
 interface ApiResponse {
-  code: number;
-  messages: string[];
-  data: {
-    custom_cocktails: Custom_Cocktails[];
-    current_page: number;
-    total_page: number;
-    total_elements: number;
-  };
-}
-
-interface Props {
-  cocktailId: string;
+  custom_cocktails: Custom_Cocktails[];
+  current_page: number;
+  total_page: number;
+  total_elements: number;
 }
 
 const token = process.env.NEXT_PUBLIC_TOKEN;
 
-export async function getData({ cocktailId }: Props) {
-  // console.log(cocktailId);
-
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/customs/${cocktailId}/custom-list?page=1`,
-    {
-      headers: {
-        Authorization: token ? `${token}` : '',
-      },
-    },
-  );
-
-  if (!response.ok) {
-    const error = new Error('Failed to fetch data');
-    throw error;
-  } else {
-    const data: ApiResponse = await response.json();
-    return data.data;
-  }
+interface Props {
+  cocktailId: number;
 }
 
-export default async function CustomCocktailList({ cocktailId }: Props) {
-  const customCocktailListData = await getData({ cocktailId });
-  const customCocktailList: Custom_Cocktails[] =
-    customCocktailListData.custom_cocktails;
+export default function CustomCocktailList(props: Props) {
+  const { cocktailId } = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [customAllData, setCustomAllData] = useState<ApiResponse>();
+  const [customList, setCustomList] = useState<Custom_Cocktails[]>([]);
 
-  const totalCount = customCocktailListData.total_elements;
-  const totalPages = customCocktailListData.total_page;
-  const currentPage = customCocktailListData.current_page;
+  const getCustomList = useCallback(
+    async (id: number) => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/customs/${id}/custom-list?page=${currentPage}`,
+        {
+          headers: {
+            Authorization: token ? `${token}` : '',
+          },
+        },
+      );
 
-  // const dummyList: DummyList[] = customCocktailList.custom_cocktails;
+      if (!response.ok) {
+        const error = new Error('Failed to fetch data');
+        throw error;
+      } else {
+        const a = await response.json();
+        const data = await a.data;
+        return data;
+      }
+    },
+    [currentPage],
+  );
+
+  const handlePageClick = async (e: { selected: number }) => {
+    if (customList) {
+      setCurrentPage(e.selected + 1);
+    }
+  };
+
+  useEffect(() => {
+    const updateCustomList = async () => {
+      const response = await getCustomList(cocktailId);
+
+      setCustomAllData(await response);
+      setCustomList(await response.custom_cocktails);
+    };
+    updateCustomList();
+  }, [currentPage, getCustomList, cocktailId]);
 
   return (
     <div>
-      {/* <CustomCocktailWriteButton /> */}
       <UpperLineBanner
         frontText="끼리코"
         secondText="님이 좋아하시는 칵테일이에요!"
       />
-      <CustomCocktailCardWrapper dummy={customCocktailList} type="big" />
+      <CustomCocktailCardWrapper dummy={customList} type="big" />
 
       <CustomCocktailPagination
-        articles={customCocktailList}
-        totalCount={totalCount}
-        totalPages={totalPages}
-        currentPage={currentPage}
-        cocktailId={cocktailId}
+        totalPages={customAllData?.total_page ?? 0}
+        handlePageClick={handlePageClick}
       />
     </div>
   );
