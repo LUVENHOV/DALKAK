1c0c470b68afe4846214bd41e7aae7393f16d2ce
백천주 <dieswing@naver.com>
2024-03-28 23:59:27 +0900

    feat: Add props in custom cocktail input component

--- frontend/components/custom-cocktail/write/CustomCocktailInput.tsx
+++ frontend/components/custom-cocktail/write/CustomCocktailInput.tsx
@@ -7,22 +7,29 @@ import styles from './CustomCocktailInput.module.scss';
 interface Props {
   max: number;
   placeText: string;
+  inputValue: string;
+  handleInputChange: ChangeEvent;
 }
 
 // interface StringNumberPair {
 //   slice(start?: number, end?: number): Array<string | number>;
 // }
 
-export default function CustomCocktailInput({ max, placeText }: Props) {
-  const [inputValue, setInputValue] = useState('');
+export default function CustomCocktailInput({
+  max,
+  placeText,
+  inputValue,
+  handleInputChange,
+}: Props) {
+  // const [inputValue, setInputValue] = useState('');
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length > max) {
-      e.target.value = e.target.value.slice(0, max);
-    }
+  // const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.value.length > max) {
+  //     e.target.value = e.target.value.slice(0, max);
+  //   }
 
-    setInputValue(e.target.value); // 입력된 값을 상태로 업데이트합니다.
-  };
+  //   setInputValue(e.target.value); // 입력된 값을 상태로 업데이트합니다.
+  // };
 
   return (
     <div className={styles.container}>
