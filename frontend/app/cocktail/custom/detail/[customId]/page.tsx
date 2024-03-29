aafb98a410b546a2e2b3b85852c2321a9a150848
유현지 <ccamy0915@naver.com>
2024-03-29 13:33:30 +0900

    fix: Modify generateStaticParams()

--- frontend/app/cocktail/custom/detail/[customId]/page.tsx
+++ frontend/app/cocktail/custom/detail/[customId]/page.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable indent */
 import CustomCocktailDetail from '@/components/custom-cocktail/detail/CustomCocktailDetail';
 
-interface ICustomType {
-  id: number;
-  image: string;
-  name: string;
-  summary: string;
-  user: {
-    id: number;
-    nickname: string;
-  };
-}
+// interface ICustomType {
+//   id: number;
+//   image: string;
+//   name: string;
+//   summary: string;
+//   user: {
+//     id: number;
+//     nickname: string;
+//   };
+// }
 
 export default function Page({ params }: { params: { customId: string } }) {
   const { customId } = params;
@@ -26,16 +27,16 @@ export async function generateStaticParams() {
   const authorization =
     'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MTEzMjkwNDUsImV4cCI6MTcxMTc2MTA0NSwiaWQiOjN9.zcY6r5AdHWBddd-sUz8oFdGV14DZLLyXi_5-BG--C20';
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/customs/${1}/custom-list?page=${1}`,
+  const json = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/customs/custom-list`,
     {
       headers: { authorization },
-      next: { tags: ['cocktailList'] },
     },
-  );
-  const json = await res.json();
-  const initialPage = (await json).data.custom_cocktails;
-  return initialPage.map((custom: ICustomType) => ({
-    customId: custom.id.toString(),
+  ).then((res) => res.json());
+
+  const initialPage = (await json).data.customIdList;
+
+  return initialPage.map((custom: number) => ({
+    customId: custom.toString(),
   }));
 }
