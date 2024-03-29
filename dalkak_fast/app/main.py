b76624cef155a55bb7201adf3f1b6a228ec3b5e1
최지희 <cjh0313c@naver.com>
2024-03-29 10:59:58 +0900

    S10P22A407-148 fix: Modify database conn

--- dalkak_fast/app/main.py
+++ dalkak_fast/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from .recommend.recommend import recommend_by_prefer
 from .recommend.recommend import recommend_by_refrigerator
 from sqlalchemy.orm import Session
-from .database.database import get_session
+from .database.database import engineconn
 
 app = FastAPI(docs_url="/fast/docs", openapi_url='/fast/openapi.json')
 
@@ -11,7 +11,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/fast/prefer-recommend/{m_id}")
-async def prefer_recommend(m_id: int, session: Session = Depends(get_session)):
+async def prefer_recommend(m_id: int, session: Session = Depends(engineconn.get_session)):
     try:
         result= recommend_by_prefer(m_id,session)
         return {"result": result}
@@ -21,7 +21,7 @@ async def prefer_recommend(m_id: int, session: Session = Depends(get_session)):
     
 
 @app.get("/fast/refrigerator-recommend/{m_id}")
-async def refrigerator_recommend(m_id:int, session: Session = Depends(get_session)):
+async def refrigerator_recommend(m_id:int, session: Session = Depends(engineconn.get_session)):
     try:
         result=recommend_by_refrigerator(m_id,session)
         return {"result": result}
