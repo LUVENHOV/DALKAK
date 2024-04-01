1169a36a2dae82965ed449ce99b69425aa8a9fb6
유현지 <ccamy0915@naver.com>
2024-04-01 18:44:59 +0900

    design: Modify navbar CSS

--- frontend/components/common/Navbar.tsx
+++ frontend/components/common/Navbar.tsx
@@ -18,43 +18,45 @@ export default function Navbar() {
   // const clearAll = memberStore((state) => state.actions.clearAll);
   return (
     <div className={styles.navbar}>
-      <Link href="/">
-        <div className="dalkak">
-          {/* <div>DAL</div>
-          <div>KAK</div> */}
-          <Image src={logo} width={1000} height={1000} alt="logo" />
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src={logo} width={100} height={100} alt="logo" />
+          </Link>
         </div>
-      </Link>
-      <Link href="/cocktail">
-        <div className={pathName?.startsWith('/cocktail') ? styles.active : ''}>
-          칵테일 목록
-        </div>
-      </Link>
+        <div className={styles.pages}>
+          <Link href="/cocktail">
+            <div
+              className={pathName?.startsWith('/cocktail') ? styles.active : ''}
+            >
+              칵테일 목록
+            </div>
+          </Link>
 
-      <Link href="/storage">
-        <div className={pathName === '/storage' ? styles.active : ''}>
-          냉장고
+          <Link href="/storage">
+            <div
+              className={pathName?.startsWith('/storage') ? styles.active : ''}
+            >
+              냉장고
+            </div>
+          </Link>
         </div>
-      </Link>
-
-      <div />
-      <div>
+      </div>
+      <div className={styles.center}>
         현재 가장 인기 있는 칵테일&nbsp;&nbsp;&nbsp;|
         <span className={styles['top-cocktail']}>
           &nbsp;&nbsp;&nbsp;1 갓파더
         </span>
       </div>
-      <div />
-      <div className={styles.hi}>
+      <div className={styles.right}>
         안녕하세요
         <span>&nbsp;&nbsp;</span>
         <span className={styles.nickname}>test</span>
         님!
+        <Link href="/member">
+          <HomeIcon />
+        </Link>
       </div>
-
-      <Link href="/member">
-        <HomeIcon />
-      </Link>
     </div>
   );
 }
