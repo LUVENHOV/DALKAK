017240b664ccf511a2b14504e7ca4b807a6bc30b
백천주 <dieswing@naver.com>
2024-04-02 00:00:25 +0900

    feat: Add navbar top rank api connection

--- frontend/components/common/NavbarTopRank.tsx
+++ frontend/components/common/NavbarTopRank.tsx
@@ -2,78 +2,51 @@
 
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 import styles from './NavbarTopRank.module.scss';
 
 interface Top_Cocktails {
-  name: string;
-  cocktailId: number;
+  cocktail_id: number;
+  cocktail_korean_name: string;
 }
 
-interface TopTen {
-  top_cocktails: Top_Cocktails[];
-}
-
-const topTen: TopTen = {
-  top_cocktails: [
-    {
-      name: '에스프레소 마티니',
-      cocktailId: 1,
-    },
-    {
-      name: '코스모폴리탄',
-      cocktailId: 2,
-    },
-    {
-      name: '보드카 크랜베리',
-      cocktailId: 3,
-    },
-    {
-      name: '애플티니',
-      cocktailId: 4,
-    },
-    {
-      name: '스키니비치',
-      cocktailId: 5,
-    },
-    {
-      name: '폰스타 마티니',
-      cocktailId: 6,
-    },
-    {
-      name: '그랜드라인 피즈',
-      cocktailId: 7,
-    },
-    {
-      name: '패션 프루트 마티니',
-      cocktailId: 8,
-    },
-    {
-      name: '블루 하와이안',
-      cocktailId: 9,
-    },
-    {
-      name: '보드카 진저 엘',
-      cocktailId: 10,
-    },
-  ],
-};
+const token = process.env.NEXT_PUBLIC_TOKEN;
 
 export default function NavbarTopRank() {
+  const router = useRouter();
   const [topCocktails, setTopCocktails] = useState<Top_Cocktails[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    setTopCocktails(topTen.top_cocktails);
+    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/recommends/heart-rank`, {
+      headers: {
+        Authorization: token ? `${token}` : '',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
+      .then((result) => {
+        // console.log(result);
+        // console.log(result.data);
+        // console.log(result.data.heart_rank_list);
+        setTopCocktails(result.data.heart_rank_list);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+
+    console.log(topCocktails);
   }, []);
 
-  //   console.log(currentIndex);
   useEffect(() => {
     const interval = setInterval(() => {
       if (currentIndex < 9) {
-        setCurrentIndex(
-          (prevIndex) => (prevIndex + 1) % topTen.top_cocktails.length,
-        );
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % 10);
       } else {
         setCurrentIndex(0);
       }
@@ -82,22 +55,23 @@ export default function NavbarTopRank() {
     return () => clearInterval(interval);
   }, []);
 
-  const currentCocktailName = topCocktails[currentIndex]?.name || '';
-  // const current = `${currentIndex + 1}  ${currentCocktailName}`;
+  const currentCocktailId = topCocktails[currentIndex + 1]?.cocktail_id;
+
+  const goToDetail = () => {
+    if (currentCocktailId !== undefined) {
+      router.push(`/cocktail/${currentCocktailId}`);
+    } else {
+      return;
+    }
+  };
+
+  const currentCocktailName =
+    topCocktails[currentIndex]?.cocktail_korean_name || '';
   const first = '';
   const current = `${currentIndex + 1}  ${currentCocktailName}`;
-  const nextCocktailName = topCocktails[currentIndex + 1]?.name || '';
+  const nextCocktailName =
+    topCocktails[currentIndex + 1]?.cocktail_korean_name || '';
   const next = `${currentIndex + 2}  ${nextCocktailName}`;
-  //   useEffect(() => {
-  //     const interval = setInterval(() => {
-  //       setCurrentIndex((prevIndex) => prevIndex + 1);
-  //     }, 1500);
-
-  //     console.log(currentIndex);
-  //     return () => clearInterval(interval);
-  //   });
-
-  //   console.log(currentIndex);
 
   return (
     <div>
@@ -105,19 +79,29 @@ export default function NavbarTopRank() {
         <div className={styles.parent}>
           {currentIndex === 0 && (
             <>
-              <div className={styles['slide-test']}>{first}</div>
-              <div className={styles['slide-test']}>{current}</div>
+              <div onClick={goToDetail} className={styles['slide-test']}>
+                {first}
+              </div>
+              <div onClick={goToDetail} className={styles['slide-test']}>
+                {current}
+              </div>
             </>
           )}
           {currentIndex !== 0 && currentIndex !== 9 && (
             <>
-              <div className={styles['slide-test']}>{current}</div>
-              <div className={styles['slide-test']}>{next}</div>
+              <div onClick={goToDetail} className={styles['slide-test']}>
+                {current}
+              </div>
+              <div onClick={goToDetail} className={styles['slide-test']}>
+                {next}
+              </div>
             </>
           )}
           {currentIndex === 9 && (
             <>
-              <div className={styles['slide-test']}>{current}</div>
+              <div onClick={goToDetail} className={styles['slide-test']}>
+                {current}
+              </div>
             </>
           )}
         </div>
