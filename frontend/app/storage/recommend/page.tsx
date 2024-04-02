5801a2918d9aee862f660e572ba3ab1105a00369
유현지 <88645538+YooHyunJi@users.noreply.github.com>
2024-04-03 03:27:27 +0900

    feat: Add spinner during recommed

--- frontend/app/storage/recommend/page.tsx
+++ frontend/app/storage/recommend/page.tsx
@@ -1,7 +1,10 @@
+'use client';
+
+import { Suspense, useEffect, useState } from 'react';
 import styles from './refrigerator-recommend.module.scss';
 
 import { ICocktailType } from '../../../type/searchTypes';
-// import CocktailCard from '@/components/cocktail-list/CocktailCard';
+import Loading from '@/components/common/Loading';
 import RecommendList from '@/components/store/RecommendList';
 
 let zeroList: ICocktailType[] = [];
@@ -18,24 +21,63 @@ const getRecommentResult = async () => {
     },
   );
   const json = await res.json();
-  // console.log(await json.data);
-  zeroList = await json.data.zero;
-  nonZeroList = await json.data.non_zero;
+  return json.data;
 };
 
 export default function Page() {
-  getRecommentResult();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // 비동기 작업 수행
+        const recommendList = await getRecommentResult();
+        zeroList = await recommendList.zero;
+        nonZeroList = await recommendList.non_zero;
+        // 데이터 처리
+        // ...
+      } catch (error) {
+        // 오류 처리
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    // fetchData 함수 호출
+  }, []);
+
   return (
     <div>
-      <RecommendList
-        title="내가 가진 재료들로 만들 수 있는 칵테일이에요"
-        cocktailList={zeroList}
-      />
-      <div className={styles['divide-line']} />
-      <RecommendList
-        title="재료가 좀 더 있으면 이런 것도 가능해요!"
-        cocktailList={nonZeroList}
-      />
+      <Suspense
+        fallback={
+          <Loading
+            text1="가지고 있는 재료로 만들 수 있는"
+            text2="최적의 칵테일을 추천해드릴게요!"
+          />
+        }
+      >
+        {loading ? (
+          <Loading
+            text1="가지고 있는 재료로 만들 수 있는"
+            text2="최적의 칵테일을 추천해드릴게요!"
+          />
+        ) : (
+          <>
+            <RecommendList
+              title="내가 가진 재료들로 만들 수 있는 칵테일이에요"
+              cocktailList={zeroList}
+            />
+            <div className={styles['divide-line']} />
+            <RecommendList
+              title="재료가 좀 더 있으면 이런 것도 가능해요!"
+              cocktailList={nonZeroList}
+            />
+          </>
+        )}
+      </Suspense>
     </div>
   );
 }
