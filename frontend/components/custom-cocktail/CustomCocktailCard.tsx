8b14b9ce0c68bcfc4180cbb70b943c66fcfc31aa
유현지 <ccamy0915@naver.com>
2024-04-03 13:12:02 +0900

    fix: Fix ESLint error

--- frontend/components/custom-cocktail/CustomCocktailCard.tsx
+++ frontend/components/custom-cocktail/CustomCocktailCard.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 import styles from './CustomCocktailCard.module.scss';
 
